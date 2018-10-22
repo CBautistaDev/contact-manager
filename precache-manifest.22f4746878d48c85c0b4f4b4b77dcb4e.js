c12219b6c46ea3f8aac94d656e1b6f0623b35ba1
Carlos Bautista <cbautista2013@gmail.com>
2018-10-22 05:31:32 -0700

    Updates

---
+++ precache-manifest.22f4746878d48c85c0b4f4b4b77dcb4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "cc654ad9e8aed23fee3a",
+    "url": "/contact-manager/static/js/main.cc654ad9.chunk.js"
+  },
+  {
+    "revision": "c980da6349b728887dd7",
+    "url": "/contact-manager/static/js/1.c980da63.chunk.js"
+  },
+  {
+    "revision": "cc654ad9e8aed23fee3a",
+    "url": "/contact-manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "c980da6349b728887dd7",
+    "url": "/contact-manager/static/css/1.8d477343.chunk.css"
+  },
+  {
+    "revision": "fff520708f2063a9e6fdf81437859ba2",
+    "url": "/contact-manager/index.html"
+  }
+];
